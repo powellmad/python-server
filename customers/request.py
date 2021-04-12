1f7d318e4ed3178277ff58f5f078835273be2b65
powellmad <madsnpowell@gmail.com>
2021-04-12 10:20:32 -0500

    worked through chapter 4 and added get and post requests for all componenets

---
+++ customers/request.py
@@ -0,0 +1,54 @@
+CUSTOMERS = [
+    {
+      "id": 1,
+      "name": "Madigan Plowell",
+      "address": "9404 Aster St",
+      "email": "mad@mad.com"
+    },
+    {
+      "id": 2,
+      "name": "Laurence Schmuler",
+      "address": "414 Iris Ave",
+      "email": "la@la.com"
+    },
+    {
+      "id": 3,
+      "name": "Shari Schyler",
+      "address": "2501 Peony Rd",
+      "email": "shar@shar.com"
+    }
+]
+
+def get_all_customers():
+    return CUSTOMERS
+
+# Function with a single parameter
+def get_single_customer(id):
+    # Variable to hold the found customer, if it exists
+    requested_customer = None
+
+    # Iterate the CUSTOMERS list above. Very similar to the
+    # for..of loops you used in JavaScript.
+    for customer in CUSTOMERS:
+        # Dictionaries in Python use [] notation to find a key
+        # instead of the dot notation that JavaScript used.
+        if customer["id"] == id:
+            requested_customer = customer
+
+    return requested_customer
+
+def create_customer(customer):
+    # Get the id value of the last employee in the list
+    max_id = CUSTOMERS[-1]["id"]
+
+    # Add 1 to whatever that number is
+    new_id = max_id + 1
+
+    # Add an `id` property to the employee dictionary
+    customer["id"] = new_id
+
+    # Add the employee dictionary to the list
+    CUSTOMERS.append(customer)
+
+    # Return the dictionary with `id` property added
+    return customer
